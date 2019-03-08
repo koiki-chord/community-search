a130efc4182d13cf60df899ddcccd0f3a17409a6
takaaki.iida <takiida0605@gmail.com>
2019-03-08 11:36:53 +0900

    community in wonderland

---
+++ src/main/kotlin/dev/koiki/chord/communitysearch/CommunitySearchApplication.kt
@@ -0,0 +1,11 @@
+package dev.koiki.chord.communitysearch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CommunitySearchApplication
+
+fun main(args: Array<String>) {
+	runApplication<CommunitySearchApplication>(*args)
+}
---
+++ src/test/kotlin/dev/koiki/chord/communitysearch/CommunitySearchApplicationTests.kt
@@ -0,0 +1,16 @@
+package dev.koiki.chord.communitysearch
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class CommunitySearchApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
