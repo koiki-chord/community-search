5e2a4888f95c1eb42b926886b28fd37c9eb58838
takaaki.iida <takiida0605@gmail.com>
2019-03-12 19:13:14 +0900

    https://github.com/koiki-chord/community-search/issues/2

--- src/main/kotlin/dev/koiki/chord/communitysearch/CommunitySearchService.kt
+++ src/main/kotlin/dev/koiki/chord/communitysearch/CommunitySearchService.kt
@@ -2,6 +2,7 @@ package dev.koiki.chord.communitysearch
 
 import brave.Tracing
 import brave.propagation.CurrentTraceContext
+import brave.propagation.TraceContext
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import org.elasticsearch.action.ActionListener
@@ -15,7 +16,6 @@ import org.elasticsearch.index.query.QueryBuilders
 import org.elasticsearch.search.SearchHit
 import org.elasticsearch.search.builder.SearchSourceBuilder
 import org.slf4j.LoggerFactory
-import org.springframework.cloud.sleuth.SpanNamer
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
 import reactor.core.publisher.MonoSink
@@ -24,8 +24,7 @@ import reactor.core.publisher.MonoSink
 class CommunitySearchService(
         val client: RestHighLevelClient,
         val mapper: ObjectMapper,
-        val tracing: Tracing,
-        val spanNamer: SpanNamer
+        val tracing: Tracing
 ) {
     private val log = LoggerFactory.getLogger(this::class.java)
 
@@ -48,24 +47,23 @@ class CommunitySearchService(
                 .indices("chord")
                 .source(searchSourceBuilder)
 
+        // this magic is needed to propagate current trace to ActionListener.
+        // don't get traceContext in ActionListener.
+        val currentTraceContext = this.tracing.currentTraceContext()
+        val traceContext = this.tracing.currentTraceContext().get()
+
         return Mono.create { sink ->
-            client.searchAsync(request, RequestOptions.DEFAULT, actionListener(sink, tracing.currentTraceContext().maybeScope(tracing.currentTraceContext().get())))
+            client.searchAsync(request, RequestOptions.DEFAULT, actionListener(sink, currentTraceContext, traceContext))
         }
     }
 
-    private fun actionListener(sink: MonoSink<List<Community>>, scope: CurrentTraceContext.Scope? = null): ActionListener<SearchResponse> =
+    private fun actionListener(sink: MonoSink<List<Community>>, currentTraceContext: CurrentTraceContext, traceContext: TraceContext): ActionListener<SearchResponse> =
             object : ActionListener<SearchResponse> {
-                // to enable Trace & Span, see details in TraceRunnable.class
-                private fun getSpan() = tracing.tracer().startScopedSpanWithParent(
-                        spanNamer.name(this, "actionListener"),
-                        tracing.currentTraceContext().get()
-                )
 
                 override fun onResponse(response: SearchResponse) {
-                    val span = getSpan()
-                    //val scope: CurrentTraceContext.Scope? = tracing.currentTraceContext().maybeScope(tracing.currentTraceContext().get())
+                    val scope = currentTraceContext.maybeScope(traceContext)
 
-                    try {
+                    scope.use {
                         if (log.isDebugEnabled)
                             log.debug("search result: ${mapper.writeValueAsString(response)}")
 
@@ -80,22 +78,17 @@ class CommunitySearchService(
                         )
 
                         sink.success(communities)
-                    } finally {
-                        span.finish()
-                        scope?.close()
                     }
                 }
 
                 override fun onFailure(e: Exception) {
-                    val span = getSpan()
+                    val scope = currentTraceContext.maybeScope(traceContext)
 
-                    try {
+                    scope.use {
                         if (log.isDebugEnabled)
                             log.error(e.message, e)
 
                         sink.error(e)
-                    } finally {
-                        span.finish()
                     }
                 }
             }
